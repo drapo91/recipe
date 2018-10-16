7fd5c80455302578e4dd5bf4bc6d04ae37153dff
drapo <drapo91@gmail.com>
2018-10-16 20:24:31 +0200

    Added ArgumentCaptor to IndexController uTest

--- src/test/java/com/drapo/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/drapo/recipe/controllers/IndexControllerTest.java
@@ -1,16 +1,22 @@
 package com.drapo.recipe.controllers;
 
 
+import com.drapo.recipe.model.Recipe;
 import com.drapo.recipe.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class IndexControllerTest {
 
@@ -30,11 +36,23 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
+        //given
+        Set<Recipe> recipes =new HashSet<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
 
+        when(recipeServiceMock.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor=ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName=indexController.getIndexPage(modelMock);
 
+        //then
         assertEquals(viewName, "index");
         verify(recipeServiceMock, Mockito.times(1)).getRecipes();
-        verify(modelMock, Mockito.times(1)).addAttribute(Mockito.eq("recipes"), Mockito.anySet());
+        verify(modelMock, Mockito.times(1)).addAttribute(Mockito.eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController=argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
