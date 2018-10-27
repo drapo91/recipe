4e493b56cb92f7f109a3600c620f71556e4d32a8
drapo <drapo91@gmail.com>
2018-10-27 16:27:12 +0200

    Corrected uTest for RecipeController

--- src/test/java/com/drapo/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/drapo/recipe/controllers/RecipeControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,6 +35,8 @@ public class RecipeControllerTest {
         recipe.setId(1L);
 
         MockMvc mockMvc= MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        doReturn(recipe).when(recipeService).findById(anyLong());
 
         mockMvc.perform(get("/recipe/show/1"))
                 .andExpect(status().isOk())
