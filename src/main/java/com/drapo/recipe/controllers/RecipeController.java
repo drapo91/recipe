1ab2185cacf2b55d6c2bdd5ca90f1e65b05d92c8
drapo <drapo91@gmail.com>
2018-10-28 13:20:03 +0100

    Possibility to add new Recipe through web form.

--- src/main/java/com/drapo/recipe/controllers/RecipeController.java
+++ src/main/java/com/drapo/recipe/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package com.drapo.recipe.controllers;
 
+import com.drapo.recipe.commands.RecipeCommand;
 import com.drapo.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +24,21 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand=recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/"+savedCommand.getId();
     }
 }
